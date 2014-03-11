91d0d9d15e2cfda53a68c5135ae7e12080119dee
Setebos <moffjerjerrod@gmail.com>
2014-03-11 20:49:19 +0100

    message de justification de ban

--- client/views/messages/messages_list.js
+++ client/views/messages/messages_list.js
@@ -11,7 +11,7 @@ Template.messagesList.helpers({
 		return Notifications.find({messageId: this._id, userId: Meteor.userId()}).fetch().length;
 	},
 	isAdmin: function() {
-		return Meteor.users.find({_id: Meteor.userId(), role: 'superadmin'}).fetch().length;
+		return (Meteor.users.find({_id: Meteor.userId(), role: 'superadmin'}).fetch().length && Meteor.userId() != this._id);
 	},
 	isBanned: function() {
 		return Meteor.users.find({_id: this._id, banned: true}).fetch().length;
@@ -22,14 +22,14 @@ Template.messagesList.events({
 	'click a.smite': function(e) {
 		var id = this._id;
 		var user = Meteor.users.findOne({_id: id}).username;
-		var content = user + " a été banni";
-		bootbox.confirm("Unleash the BANHAMMER ?", function(result) {
-			if(result == true) {
+		bootbox.prompt("Unleash the BANHAMMER ?", function(result) {
+			if(result !== null && result != '') {
 				Meteor.call('smite', id, function(error,id) {	
 					if(error) {
 						throwError(error.reason);
 					}
 				});
+				var content = user + " a été banni avec la justification suivante : " + result;
 				Meteor.call('postBanMessage', content, function(error, id) {
 					if(error) {
 						throwError(error.reason);
